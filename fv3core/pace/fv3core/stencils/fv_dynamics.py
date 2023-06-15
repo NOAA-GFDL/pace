a3a3c1d4bbb46c62e8b6205e61a3308e044e6adb
Florian Deconinck <deconinck.florian@gmail.com>
2023-06-15 12:29:13 -0400

    Verbose runtime error when bad dt_atmos

--- fv3core/pace/fv3core/stencils/fv_dynamics.py
+++ fv3core/pace/fv3core/stencils/fv_dynamics.py
@@ -173,7 +173,10 @@ class DynamicalCore:
             dace_compiletime_args=["state"],
         )
         if timestep == timedelta(seconds=0):
-            raise RuntimeError("Bad dynamical core configuration: bdt is 0")
+            raise RuntimeError(
+                "Bad dynamical core configuration:"
+                " the atmospheric timestep is 0 seconds!"
+            )
         # nested and stretched_grid are options in the Fortran code which we
         # have not implemented, so they are hard-coded here.
         self.call_checkpointer = checkpointer is not None
