d927d683870e053af477253ea884112eece90410
Oliver Elbert <Oliver.Elbert@noaa.gov>
2023-07-14 09:09:30 -0400

    updated repo to NOAA

--- examples/build_scripts/build_gaea_c5.sh
+++ examples/build_scripts/build_gaea_c5.sh
@@ -13,7 +13,7 @@ module load boost/1.79.0
 module load python/3.9
 
 # clone Pace and update submodules
-git clone --recursive https://github.com/ai2cm/pace
+git clone --recursive https://github.com/NOAA-GFDL/pace
 cd pace
 
 # create a conda environment for pace
