0a8302a23fa25a572b3ab22f5b5ef90ba0cb4925
Oliver Elbert <oliver.elbert36@gmail.com>
2023-09-01 12:00:08 -0400

    initial commit

--- stencils/pace/stencils/testing/test_translate.py
+++ stencils/pace/stencils/testing/test_translate.py
@@ -327,6 +327,12 @@ def get_communicator(comm, layout):
     return communicator
 
 
+def get_tile_communicator(comm, layout):
+    partitioner = pace.util.TilePartitioner(layout)
+    communicator =  pace.util.TileCommunicator(comm, partitioner)
+    return communicator
+
+
 @pytest.mark.parallel
 @pytest.mark.skipif(
     MPI is None or MPI.COMM_WORLD.Get_size() == 1,
@@ -343,11 +349,18 @@ def test_parallel_savepoint(
     compute_grid,
     xy_indices=True,
 ):
-    layout = (
-        int((MPI.COMM_WORLD.Get_size() // 6) ** 0.5),
-        int((MPI.COMM_WORLD.Get_size() // 6) ** 0.5),
-    )
-    communicator = get_communicator(MPI.COMM_WORLD, layout)
+    if MPI.COMM_WORLD.Get_size()%6 != 0:
+        layout = (
+            int(MPI.COMM_WORLD.Get_size() ** 0.5),
+            int(MPI.COMM_WORLD.Get_size() ** 0.5),
+        )
+        communicator = get_tile_communicator(MPI.COMM_WORLD, layout)
+    else:
+        layout = (
+            int((MPI.COMM_WORLD.Get_size() // 6) ** 0.5),
+            int((MPI.COMM_WORLD.Get_size() // 6) ** 0.5),
+        )
+        communicator = get_communicator(MPI.COMM_WORLD, layout)
     caplog.set_level(logging.DEBUG, logger="fv3core")
     if case.testobj is None:
         pytest.xfail(
