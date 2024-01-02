6624a92042810937b2dce1a12e9e8c16bdfb9ac7
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2024-01-02 11:35:49 -0500

    rm safety checks

--- tests/main/grid/test_eta.py
+++ tests/main/grid/test_eta.py
@@ -43,31 +43,33 @@ def test_set_hybrid_pressure_coefficients_79_correct():
     if not np.array_equal(bk_answers, bk_results):
         raise ValueError("Unexpected value of bk")
 
+    driver.safety_checker.clear_all_checks()
 
-# def test_set_hybrid_pressure_coefficients_91_correct():
-#
-#    config_file = "tests/main/grid/input/test_config_91.yaml"
-#    with open(config_file, "r") as f:
-#        yaml_config = yaml.safe_load(f)
-#
-#    driver_config = pace.driver.DriverConfig.from_dict(yaml_config)
-#    driver_config.comm_config = pace.driver.NullCommConfig(rank=0, total_ranks=6)
-#    driver = pace.driver.Driver(config=driver_config)
-#
-#    p_results = driver.state.grid_data.p.data
-#    ak_results = driver.state.grid_data.ak.data
-#    bk_results = driver.state.grid_data.bk.data
-#    ak_answers, bk_answers = set_answers(config_file)
-#
-#    if ak_answers.size != ak_results.size:
-#        raise ValueError("Unexpected size of bk")
-#    if bk_answers.size != bk_results.size:
-#        raise ValueError("Unexpected size of ak")
-#
-#    if not np.array_equal(ak_answers, ak_results):
-#        raise ValueError("Unexpected value of ak")
-#    if not np.array_equal(bk_answers, bk_results):
-#        raise ValueError("Unexpected value of bk")
+
+def test_set_hybrid_pressure_coefficients_91_correct():
+
+    config_file = "tests/main/grid/input/test_config_91.yaml"
+    with open(config_file, "r") as f:
+        yaml_config = yaml.safe_load(f)
+
+    driver_config = pace.driver.DriverConfig.from_dict(yaml_config)
+    driver_config.comm_config = pace.driver.NullCommConfig(rank=0, total_ranks=6)
+    driver = pace.driver.Driver(config=driver_config)
+
+    p_results = driver.state.grid_data.p.data
+    ak_results = driver.state.grid_data.ak.data
+    bk_results = driver.state.grid_data.bk.data
+    ak_answers, bk_answers = set_answers(config_file)
+
+    if ak_answers.size != ak_results.size:
+        raise ValueError("Unexpected size of bk")
+    if bk_answers.size != bk_results.size:
+        raise ValueError("Unexpected size of ak")
+
+    if not np.array_equal(ak_answers, ak_results):
+        raise ValueError("Unexpected value of ak")
+    if not np.array_equal(bk_answers, bk_results):
+        raise ValueError("Unexpected value of bk")
 
 
 @pytest.mark.xfail
