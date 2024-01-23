d263bc2eae2a38a7c0554976a07f12f7c7f62f99
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2024-01-23 12:41:04 -0500

    use pytest parametrize for the xfail tests

--- tests/main/grid/test_eta.py
+++ tests/main/grid/test_eta.py
@@ -64,15 +64,3 @@ def test_set_hybrid_pressure_coefficients_nofile(cfile):
     driver_config = pace.driver.DriverConfig.from_dict(yaml_config)
     driver_config.comm_config = pace.driver.NullCommConfig(rank=0, total_ranks=6)
     driver = pace.driver.Driver(config=driver_config)
-
-
-# @pytest.mark.xfail
-# def test_set_hybrid_pressure_coefficients_nonmonotonic():
-#
-#    config_file = "tests/main/grid/input/test_config_not_mono.yaml"
-#    with open(config_file, "r") as f:
-#        yaml_config = yaml.safe_load(f)
-#
-#    driver_config = pace.driver.DriverConfig.from_dict(yaml_config)
-#    driver_config.comm_config = pace.driver.NullCommConfig(rank=0, total_ranks=6)
-#    driver = pace.driver.Driver(config=driver_config)
