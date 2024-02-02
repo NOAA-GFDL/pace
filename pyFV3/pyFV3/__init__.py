455d2debe05ca4a4eb4638752f3a42b40701d753
Frank Malatino <frank.malatino@noaa.gov>
2024-02-02 11:39:51 -0500

    Deleted local pyFV3

--- pyFV3/pyFV3/__init__.py
+++
@@ -1,8 +0,0 @@
-from ._config import DynamicalCoreConfig
-from .dycore_state import DycoreState
-from .stencils.fv_dynamics import DynamicalCore
-from .stencils.fv_subgridz import DryConvectiveAdjustment
-from .wrappers.geos_wrapper import GeosDycoreWrapper
-
-
-__version__ = "0.2.0"
