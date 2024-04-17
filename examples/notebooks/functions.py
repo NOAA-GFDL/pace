81ddaa700def2d81a92d0eafcf646073420b5ecd
Oliver Elbert <oliver.elbert36@gmail.com>
2024-04-17 17:12:09 -0400

    fixed notebooks too

--- examples/notebooks/functions.py
+++ examples/notebooks/functions.py
@@ -267,7 +267,7 @@ def configure_domain(
     quantity_factory = QuantityFactory.from_backend(sizer=sizer, backend=backend)
 
     metric_terms = MetricTerms(
-        quantity_factory=quantity_factory, communicator=communicator
+        quantity_factory=quantity_factory, communicator=communicator, eta_file="eta79.nc"
     )
 
     # workaround for single layer
