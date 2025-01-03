8c8166e0dd472635f9433dce7ee17ea49f429da2
Frank Malatino <frank.malatino@noaa.gov>
2025-01-03 10:54:30 -0500

    Updated c5 build script to match new methods

--- examples/build_scripts/build_gaea_c5_gcc.sh
+++ examples/build_scripts/build_gaea_c5_gcc.sh
@@ -1,22 +1,18 @@
 #!/usr/bin/env bash
 
-# Example bash script to install Pace to run bare-metal on Gaea's c4 cluster
+# Example bash script to install Pace to run bare-metal on Gaea's c5 cluster
 
 set -e -x
 
 # module load necessary system software
+module use -a /usw/conda/modulefiles
 module rm PrgEnv-intel
 module load PrgEnv-gnu
-module rm gcc
-module load gcc/12.2.0
-module load boost/1.79.0
-module load python/3.11.7
+module load gcc-native/12.3
+module load boost/1.85.0
+module load miniforge
 
-export CC=`which gcc`
-export CXX=`which g++`
-export MPICC=`which mpicc`
-export MPICXX=`which mpicxx`
-export DACE_compiler_cpu_executable=${CXX}
+export DACE_compiler_cpu_executable='which g++'
 export GT4PY_EXTRA_COMPILE_ARGS="-O3 -ffast-math -fprefetch-loop-arrays -funroll-all-loops"
 export OPENMP_CPPFLAGS="-fopenmp -fopenmp-simd"
 export OPENMP_LDFLAGS="-fopenmp -fopenmp-simd"
