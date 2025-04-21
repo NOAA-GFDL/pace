1ba1f48a3cb69a0d3c2297d28582a09c69596a6d
Janice Kim <janice.kim@noaa.gov>
2025-04-21 11:33:22 -0400

    Rossby initialization test + netcdf data

---
+++ tests/main/fv3core/test_dycore_rossby_init.py
@@ -0,0 +1,166 @@
+""" Unit test Rossby-Haurwitz wave 4 initialization
+
+Corresponds to Fortran shallow-water test #6 found in tools/test_cases.F90 of
+https://github.com/NOAA-GFDL/GFDL_atmos_cubed_sphere.git 
+"""
+
+import os
+import unittest.mock
+from dataclasses import fields
+from typing import Tuple
+import xarray as xr
+import numpy as np
+import matplotlib.pyplot as plt
+
+import pyFV3.initialization.analytic_init as ai
+from ndsl import (
+    CubedSphereCommunicator,
+    CubedSpherePartitioner,
+    NullComm,
+    Quantity,
+    QuantityFactory,
+    SubtileGridSizer,
+    TilePartitioner,
+)
+from ndsl.grid import GridData, MetricTerms
+from ndsl.performance.timer import NullTimer, Timer
+from pyFV3 import DycoreState, DynamicalCoreConfig
+
+
+DIR = os.path.abspath(os.path.dirname(__file__))
+PACE_DIR = os.path.join(DIR, "..", "..", "..")
+
+
+def setup_dycore_state() -> DycoreState:
+    backend = "numpy"
+    config = DynamicalCoreConfig(
+        layout=(1, 1),
+        npx=13,
+        npy=13,
+        npz=79,
+        ntiles=6,
+        nwat=6,
+        dt_atmos=225,
+        a_imp=1.0,         # TODO: What is fortran equiv?
+        beta=0.0,
+        consv_te=False,  # not implemented, needs allreduce
+        d2_bg=0.0,
+        d2_bg_k1=0.2,      # TODO: What is fortran equiv?
+        d2_bg_k2=0.1,      # TODO: What is fortran equiv?
+        d4_bg=0.15,
+        d_con=1.0,         # TODO: What is fortran equiv?
+        d_ext=0.0,         # TODO: What is fortran equiv?
+        dddmp=0.0,
+        delt_max=0.002,    # TODO: What is fortran equiv?
+        do_sat_adj=True,   # TODO: What is fortran equiv?
+        do_vort_damp=True, # TODO: What is fortran equiv?
+        fill=True,         # TODO: What is fortran equiv?
+        hord_dp=6,
+        hord_mt=6,
+        hord_tm=6,
+        hord_tr=8,
+        hord_vt=6,
+        hydrostatic=False, # TODO: What is fortran equiv?
+        k_split=1,         # TODO: What is fortran equiv?
+        ke_bg=0.0,         # TODO: What is fortran equiv?
+        kord_mt=9,         # TODO: What is fortran equiv?
+        kord_tm=-9,        # TODO: What is fortran equiv?
+        kord_tr=9,         # TODO: What is fortran equiv?
+        kord_wz=9,         # TODO: What is fortran equiv?
+        n_split=1,
+        nord=3,
+        p_fac=0.05,        # TODO: What is fortran equiv?
+        rf_fast=True,      # TODO: What is fortran equiv?
+        rf_cutoff=3000.0,  # TODO: What is fortran equiv?
+        tau=10.0,          # TODO: What is fortran equiv?
+        vtdm4=0.06,        # TODO: What is fortran equiv?
+        z_tracer=True,     # TODO: What is fortran equiv?
+        do_qa=True,        # TODO: What is fortran equiv?
+    )
+    mpi_comm = NullComm(
+        rank=0, total_ranks=6 * config.layout[0] * config.layout[1], fill_value=0.0
+    )
+    partitioner = CubedSpherePartitioner(TilePartitioner(config.layout))
+    communicator = CubedSphereCommunicator(mpi_comm, partitioner)
+    sizer = SubtileGridSizer.from_tile_params(
+        nx_tile=config.npx - 1,
+        ny_tile=config.npy - 1,
+        nz=config.npz,
+        n_halo=3,
+        extra_dim_lengths={},
+        layout=config.layout,
+        tile_partitioner=partitioner.tile,
+        tile_rank=communicator.tile.rank,
+    )
+    quantity_factory = QuantityFactory.from_backend(sizer=sizer, backend=backend)
+    eta_file = "tests/main/input/eta79.nc"
+    metric_terms = MetricTerms(
+        quantity_factory=quantity_factory,
+        communicator=communicator,
+        eta_file=eta_file,
+    )
+    grid_data = GridData.new_from_metric_terms(metric_terms)
+
+    # create an initial state for the Rossby Wave number 4 test case
+    state = ai.init_analytic_state(
+        analytic_init_case="rossby",
+        grid_data=grid_data,
+        quantity_factory=quantity_factory,
+        adiabatic=config.adiabatic,
+        hydrostatic=config.hydrostatic,
+        moist_phys=config.moist_phys,
+        comm=communicator,
+    )
+    return state
+
+
+def plot_2d_diff(attribute, ds_values, state_values):
+    plt.title(f"Diff NetCDF vs Pace Dycore State '{attribute}'")
+    diff = ds_values - state_values
+    plt.imshow(diff, cmap='viridis')
+    plt.colorbar()
+    plt.savefig(f"rossby_diff_{attribute}.png") # TODO: directory somewhere?
+    plt.clf()
+    
+    
+def test_rossby_init_validation():
+    state = setup_dycore_state()
+
+    # Read in netcdf file as dataset
+    validation_dir = os.path.join(PACE_DIR, "tests", "main", "data", "rossby_validation", "zero_time_restart")
+    core1_ds = xr.open_dataset(os.path.join(validation_dir, "fv_core.res.tile1.nc"))
+    
+    max_eps_error = 1e-10
+    
+    # Set to True to generate 2d heatmap, comparing dycore state values with dataset
+    plot = True 
+
+    # 3D/2D Attributes
+    for attribute in ["u", "v", "delp", "phis"]:
+        print(f"{attribute}:")
+        state_values = getattr(state, attribute).view[:]
+        state_ndims = len(getattr(state, attribute).dims)
+
+        if state_ndims == 2:
+            core1_ds_values = core1_ds[attribute].values[0, :].transpose(1, 0)
+            core1_ds_values_2d, state_values_2d = core1_ds_values, state_values
+        elif state_ndims == 3:
+            core1_ds_values = core1_ds[attribute].values[0, :].transpose(2, 1, 0)
+            core1_ds_values_2d, state_values_2d = core1_ds_values[:,:,0], state_values[:,:,0]
+        else:
+            assert False, f"Unexpected number of dims in DycoreState {attribute}"
+        
+        if plot==True:
+            plot_2d_diff(attribute, core1_ds_values_2d, state_values_2d)
+        
+        max_error_diff = np.max(np.absolute(core1_ds_values - state_values))
+        assert max_error_diff < max_eps_error
+        # TODO: Use assert_almost_equal instead?
+        np.testing.assert_almost_equal(state_values, core1_ds_values, decimal=11)
+
+    # NOTE: The original test_cases.F90 initialized tracers for cl and cl2,
+    #       but we do not initialize or check for them in this test.
+
+    # TODO: Do we need sphum from fv_tracer.res.tile*.nc?
+    # TODO: Do we need u_srf, v_srf from fv_srf_wnd.res.tile*.nc?
+    # TODO: Do we need to check all tiles 1-6?
