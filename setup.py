d94694b4a7ac37bac6b99fe90f7cdf2685c244da
Frank Malatino <frank.malatino@noaa.gov>
2025-05-22 10:00:07 -0400

    Adding conditional statement for checking of local clone of dependency

--- setup.py
+++ setup.py
@@ -1,4 +1,33 @@
+import os
+from pathlib import Path
+from typing import List
+
 from setuptools import setup
 
 
-setup()
+def find_pkg(name: str, relative_path: str) -> str:
+    """
+    Returns an absolute path to a local package or
+    checks out repository from GitHub.
+    """
+    dependecy_path = f"{Path(os.path.abspath(__file__)).parent / relative_path}"
+    if os.path.isdir(dependecy_path):
+        return (
+            f"{name} @ file://{Path(os.path.abspath(__file__)).parent / relative_path} "
+        )
+    else:
+        return f"{name} @ git+https://github.com/NOAA-GFDL/{name}.git@develop"
+
+
+requirements: List[str] = [
+    find_pkg("NDSL", "NDSL"),
+    find_pkg("pyFV3", "pyFV3"),
+    find_pkg("pySHiELD", "pySHiELD"),
+    "dacite",
+    "f90nml",
+    "numpy < 2.0.0",
+    "xarray",
+    "zarr < 3.0.0",
+]
+
+setup(install_requires=requirements)
