e8b7e7eb53f313c060cd0f761e1bb19f3e9e5e96
Frank Malatino <frank.malatino@noaa.gov>
2025-05-27 14:44:39 -0400

    Reverting back to using find_pkg in setup.py

--- setup.py
+++ setup.py
@@ -5,24 +5,15 @@ from typing import List
 from setuptools import setup
 
 
-def find_pkg(name: str, relative_path: str) -> str:
-    """
-    Returns an absolute path to a local package or
-    provides location of Github repository.
-    """
-    dependecy_path = f"{Path(os.path.abspath(__file__)).parent / relative_path}"
-    if os.path.isdir(dependecy_path):
-        return (
-            f"{name} @ file://{Path(os.path.abspath(__file__)).parent / relative_path} "
-        )
-    else:
-        return f"{name} @ git+https://github.com/NOAA-GFDL/{name}.git@develop"
+def local_pkg(name: str, relative_path: str) -> str:
+    """Returns an absolute path to a local package."""
+    return f"{name} @ file://{Path(os.path.abspath(__file__)).parent / relative_path} "
 
 
 requirements: List[str] = [
-    find_pkg("NDSL", "NDSL"),
-    find_pkg("pyFV3", "pyFV3"),
-    find_pkg("pySHiELD", "pySHiELD"),
+    local_pkg("NDSL", "NDSL"),
+    local_pkg("pyFV3", "pyFV3"),
+    local_pkg("pySHiELD", "pySHiELD"),
     "dacite",
     "f90nml",
     "numpy < 2.0.0",
