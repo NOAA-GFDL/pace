482c490ba0b6cb33857e55ac56f21bf3b1a06aae
Florian Deconinck <deconinck.florian@gmail.com>
2025-10-23 12:55:55 -0400

    Typo + one more normalizing

--- tests/main/driver/test_diagnostics_config.py
+++ tests/main/driver/test_diagnostics_config.py
@@ -67,7 +67,7 @@ def test_zselect_raises_error_if_3rd_dim_not_z(tmpdir):
         quantity_factory = QuantityFactory.from_backend(
             sizer=SubtileGridSizer.from_tile_params(
                 nx_tile=12,
-                ny_tuke=12,
+                ny_tile=12,
                 nz=79,
                 n_halo=3,
                 extra_dim_lengths={},
