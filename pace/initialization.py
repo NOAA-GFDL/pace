a0befc9ec74d5093ea4ab0589e85e7095f72be75
Frank Malatino <frank.malatino@noaa.gov>
2026-03-13 09:59:32 -0400

    Reverting back to full typing

--- pace/initialization.py
+++ pace/initialization.py
@@ -91,7 +91,7 @@ class InitializerSelector(Initializer):
         )
 
     @classmethod
-    def from_dict(cls, config: dict, hooks: dict = None):
+    def from_dict(cls, config: dict, hooks: dict | None = None):
         if not hooks:
             hooks = {}
         instance = cls.registry.from_dict(config, hooks=hooks)
